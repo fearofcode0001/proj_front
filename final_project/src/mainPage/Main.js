1c02b5c84e20a14c78163e19779b1668659743f4
fearofgod0001 <dlxo8888@gmail.com>
2023-05-30 12:01:49 +0900

    0530_test

--- final_project/src/mainPage/Main.js
+++ final_project/src/mainPage/Main.js
@@ -1,19 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import bckimg from "../img/fog.jpg"
 
-const Body =styled.div`
-width: 100%;
-height: 100vh;
-background-color: rgb(28,20,243);
+const Container =styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+const Head = styled.div`
+    width: 100%;
+    height: 180px;
+    background-color: rgb(28,20,243);
+    display: flex;
 
 `
-
+const Body = styled.div`
+    width: 100%;
+    height: 100%;
+`
+const Foot = styled.div`
+    width: 100%;
+    height: 180px;
+    background-color: rgb(28,20,243);
+`
 const Main= () =>{
     
     return(
-        <Body>
+        <Container>
+            <Head>
+            </Head>
+            <Body>                
+            </Body>
+            <Foot>
+            </Foot>
 
-        </Body>
+        </Container>
 
     );
 };
