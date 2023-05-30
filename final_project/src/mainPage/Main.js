90f763ee10a2fbc5389a8bdbc8cc8f7d179523bd
fearofgod0001 <dlxo8888@gmail.com>
2023-05-30 13:53:09 +0900

    0530

--- final_project/src/mainPage/Main.js
+++ final_project/src/mainPage/Main.js
@@ -7,51 +7,82 @@ const Container =styled.div`
     height: 100vh;
     display: flex;
     flex-direction: column;
+    overflow:scroll;
+    ::-webkit-scrollbar {
+	display:none
+    }
 `
+
 const Head = styled.div`
     width: 100%;
     height: 180px;
-    background-color: rgb(28,20,243);
     display: flex;
-
+    flex-direction: column;
+    padding: 0 0 0 20px;
+    .top{
+        height: 70px;
+        font-weight: bolder;
+        font-size: 50px;
+    }
+    .bottom{
+        color: rgb(100,100,100);
+        font-weight: bolder;
+        cursor: pointer;
+        
+    }
 `
+
 const Body = styled.div`
     width: 100%;
     height: 100%;
     //이미지를 사용 하려면  ${0}양식 사용
     background-image:url(${bckimg});
     background-size: contain;
-    animation: transX 3s ease-in-out; 
+    background-repeat: none;
+    animation: transX 10s; 
     //애니메이션 무한반복
     animation-iteration-count: infinite;
     @keyframes transX {
         0% {
-          transform: translateX(400px); // 이 top 기준으로 400px 내려간 곳에서 시작할거다
-
+          transform: translateX(800px); // 이 top 기준으로 400px 내려간 곳에서 시작할거다
         }
         100% {
-          transform: translateX(0px);   // 그리고 도착지는 원래 지점
-
+          transform: translateX(-800px);   // 그리고 도착지는 원래 지점
         }
-      }
-   
+      } 
+    overflow:scroll;
+    ::-webkit-scrollbar {
+	display:none /* Chrome , Safari , Opera */
+}
 `
+
 const Foot = styled.div`
     width: 100%;
     height: 180px;
-    background-color: rgb(28,20,243);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 9px;
+    color: rgb(100,100,100);
 `
+
 const Main= () =>{
     
     return(
         <Container>
             <Head>
+                <div className="top">
+                    iMMUTABLE
+                </div>
+                <div className="bottom">
+                   menu
+                </div>
             </Head>
             <Body>                
             </Body>
             <Foot>
+                copyrightsⓒ
             </Foot>
-
         </Container>
 
     );
