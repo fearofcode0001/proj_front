ef77f6baf3c327453c2f5600ab4655f4a866f9b7
fearofgod0001 <dlxo8888@gmail.com>
2023-05-30 12:17:45 +0900

    0530_CSS

--- final_project/src/mainPage/Main.js
+++ final_project/src/mainPage/Main.js
@@ -7,10 +7,6 @@ const Container =styled.div`
     height: 100vh;
     display: flex;
     flex-direction: column;
-    //이미지를 사용 하려면  ${0}양식 사용
-    background-image:url(${bckimg});
-    background-size: contain;
-    background-repeat: no-repeat;
 `
 const Head = styled.div`
     width: 100%;
@@ -22,6 +18,21 @@ const Head = styled.div`
 const Body = styled.div`
     width: 100%;
     height: 100%;
+        //이미지를 사용 하려면  ${0}양식 사용
+    background-image:url(${bckimg});
+    background-size: contain;
+    animation: transY 2s ease-in-out; 
+    @keyframes transY {
+        0% {
+          transform: translateY(400px); // 이 top 기준으로 400px 내려간 곳에서 시작할거다
+          opacity: 0; // 처음에는 가려놓고 
+        }
+        100% {
+          transform: translateY(0px);   // 그리고 도착지는 원래 지점
+          opacity: 1; // 마지막에 슬슬 보여주기
+        }
+      }
+   
 `
 const Foot = styled.div`
     width: 100%;
