4d2b42b607e65653e301e08eec179cf10c8f385a
Junny990316 <heojueun0316@gmail.com>
2023-06-01 14:57:35 +0900

    위치 안맞음

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,58 +1,93 @@
-import React from "react";
+import React, { useState ,useContext } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+
+
+const Container = styled.div`
+height: 100vh;
+display: flex;
+justify-content: space-evenly;
+text-align: center;
+overflow-y: scroll;
+`;
+const InerContainer = styled.div`
+align-items: center;
+width: 400px;
+.top{
+    margin-top: 150px;
+    font-weight: bolder;
+    font-size: 50px;
+}
+.input {
+    margin-top: 30px;
+}
+input {
+    width: 100%;
+    height: 40px;
+    margin-top: 20px;
+    font-weight: bolder;
+    font-size: 10px;
+    border: 1px solid #ccc;
+    &::placeholder {
+        padding: 5px;
+        font-size: 12px;
+    };
+}
+
+.email,
+.verify {
+    width: 80%;
+}
+
+.emailBtn,
+.verifyBtn { // 위치가 안맞아요 고쳐주세요 ;(
+    width: 15%;
+    height: 40px;
+    margin-left: 5px;
+    font-size: 12px;
+    font-weight: bolder;
+    background-color: white;
+    border: 1px solid #ccc;
+}
+.singUp {
+    width: 100%;
+    height: 38px;
+    margin-top: 20px;
+    background-color: white;
+}
+.goToLogin {
+    width: 100%;
+    height: 38px;
+    margin-top: 50px;
+    background-color: white;
+}
+.enable-button {
+
+}
+.enable-button:active {
+
+}
+.disable-button {
+
+}
+`;
 
 const SignUp = () => {
-    const Container = styled.div`
-    height: 100vh;
-    display: flex;
-    justify-content: space-evenly;
-    text-align: center;
-    overflow-y: scroll;
-    `;
-    const InerContainer = styled.div`
-    align-items: center;
-    .top{
-        margin-top: 150px;
-        font-weight: bolder;
-        font-size: 50px;
-    }
-    .input {
-        margin-top: 30px;
-    }
-    input {
-        width: 250px;
-        height: 35px;
-        margin-top: 10px;
-        font-weight: bolder;
-    }
-    .email {
-        width: 197px;
-    }
-    .verify {
-        width: 197px;
-    }
-    .emailBtn {
-        width: 50px;
-        height: 38px;
-        margin-left: 3px;
-        font-weight: bolder;
-        background-color: white;
-    }
-    .verifyBtn {
-        width: 50px;
-        height: 38px;
-        margin-left: 3px;
-        font-weight: bolder;
-        background-color: white;
-    }
-    .singUp {
-        width: 258px;
-        height: 38px;
-        margin-top: 20px;
-        background-color: white;
-        font-weight: bolder;
-    }
-    `;
+   
+    const navigate = useNavigate();
+
+    // 키보드 입력 받기
+    const [inputName, setInputName] = useState("");
+    const [inputEMail, setInputEmail] = useState("");
+    const [code, setCode] = useState("");
+    const [inputPw, setInputPw] = useState("")
+    const [inputConPw, setInputConPw] = useState("");
+    const [inputPhone, setInputPhone] = useState("");
+
+    // 오류 메세지
+
+   
     return(
         <Container>
             <InerContainer>
@@ -61,13 +96,18 @@ const SignUp = () => {
                 </div>
                 <div className="input">
                     <div className="item">
+                        <input type="text" placeholder="NAME"/>
+                    </div>
+                
+                    <div className="item1">
                         <input className="email" type="email" placeholder="EMAIL"/>
                         <button className="emailBtn">전송</button>
                     </div>
-                    <div className="item">
+                    <div className="item1">
                         <input className="verify" type="text" placeholder="VERIFYCODE"/>
                         <button className="verifyBtn">인증</button>
                     </div>
+
                     <div className="item">
                         <input type="password" placeholder="PWD"/>
                     </div>
@@ -81,7 +121,10 @@ const SignUp = () => {
                         <input type="address" placeholder="ADDRESS"/>
                     </div>
                     <div>
-                        <button className="singUp">회원가입</button>
+                        <button className="singUp">CREATE</button>
+                    </div>
+                    <div>
+                        <button className="goToLogin">LOGIN</button>
                     </div>
                 </div>
                 
