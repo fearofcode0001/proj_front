9d2a786809374d299b4283dbf42944f75eb575ca
fearofgod0001 <dlxo8888@gmail.com>
2023-06-01 12:46:31 +0900

    0601

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container =styled.div`
+    width: 100%;
+    height: 100vh;
+`
+
+const Login =()=>{
+
+    return(
+        <Container>
+        </Container>
+    );
+};
+
+export default Login;
