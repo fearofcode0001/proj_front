678c854f8fc6666a23804814598ff199959a8b7d
LEETAESUK <dlxo8888@gmail.com>
2023-06-01 17:06:56 +0900

    0601

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Container=styled.div`
+    width: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
