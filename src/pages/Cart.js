bc17440085d599fcdf6993145fb276ce78e80c84
fearofgod0001 <dlxo8888@gmail.com>
2023-06-02 12:43:36 +0900

    0602

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -15,17 +15,27 @@ const MainBody=styled.div`
     flex-direction: column;
     border: 1px solid black;
     width: 80%;
-    height: 1000px;
+    height: 90%;
 `
 const Products = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    border: 1px solid black;
     width: 100%;
     height: 50%;
     overflow-y:scroll;
+    ::-webkit-scrollbar {
+    display: none;
+    }
+
+`
+const Products_in=styled.div`
+    width: 100%;
+    height: 150px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    &:hover{
+        background-color: rgba(0,0,0,0.1);
+    }
 `
 const Total=styled.div`
     display: flex;
@@ -40,7 +50,6 @@ const OrderInfo=styled.div`
     align-items: center;
     width: 100%;
     height: 30%;
-    border: 1px solid black;
 `
 
 
@@ -50,6 +59,20 @@ const Cart=()=>{
         <Container>        
             <MainBody>
                 <Products>
+                    <Products_in>
+                        <input type="checkbox" />
+                        <img src ="#"/>
+                        <div className="itemName">Sweat Shirts</div>
+                        <div className="count">
+                            <input type="text"/>
+                            <div className="countbutton">
+                                <button className="plus">∧</button>
+                                <button className="minus">∨</button>
+                            </div>
+                        </div>
+                        <div className="price">159,000 won</div>
+
+                    </Products_in>
 
                 </Products>
                 <Total>
