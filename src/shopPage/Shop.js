baa9a62d0719c0af86f4718bd3bf61aaf825e16b
d_kyu <dkrara93@naver.com>
2023-06-05 14:42:29 +0900

    Page 이동경로 추가

--- src/shopPage/Shop.js
+++ src/shopPage/Shop.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 import DropdownMenu from "./DropdownMenu";
+import { useNavigate, Link } from "react-router-dom";
 import test from "../img/shop.webp"
 
 const Container = styled.div`
@@ -30,6 +31,13 @@ const Head = styled.div`
     height: 80px;
     display: flex;
     flex-direction: column;
+
+    a{
+        text-decoration: none;
+        color: black;
+    }
+
+
 
     .nav{
         padding: 0 20px 0 10px;
@@ -135,7 +143,7 @@ const MenuList = [
 const IsLoginFalse = [
     { name : "login"}
   ]
-const IsLoginTrue = [
+  const IsLoginTrue = [
     { name : "logout"},
     { name : "mypage"},    
     { name : "cart"},    
@@ -149,6 +157,22 @@ const Shop = () => {
     const [selectedMenu, setSelectedMenu] = useState(null)
     const [isLogin, setIsLogin] = useState(true);
     const [isMenuClicked, setIsMenuClicked] = useState(false);
+    const navigate = useNavigate();
+
+    const onChangePage=(e)=>{
+        console.log(e);
+        if(e==="cart"){
+            navigate("/Cart")
+        }
+        else if(e==="logout"){
+            setIsLogin(false);
+        }
+        else if(e==="SHOP"){
+            navigate("/Shop");
+            console.log(e);
+        }
+    }
+
     
     const handleMenuClick = (menuName) => {
         if (selectedMenu === menuName) {
@@ -175,17 +199,17 @@ const Shop = () => {
                           </div>
                         ))}
                     </div>
-                    <div className="nav2">
+                    <a href="/"><div className="nav2">
                         iMMUTABLE
-                    </div>
+                    </div></a>
                     <div className="nav3">
                     {IsLoginFalse.map(s=>( isLogin===false &&
                                         <TopButton key={s.name}>
-                                            {s.name}
+                                            <Link to="/Login">{s.name}</Link>
                                         </TopButton>
                                     ))}
-                          {IsLoginTrue.map(s=>( isLogin===true &&
-                                        <TopButton key={s.name}>
+                           {IsLoginTrue.map(s=>( isLogin===true &&
+                                        <TopButton key={s.name} onClick={()=>onChangePage(s.name)}>
                                             {s.name}
                                         </TopButton>
                                     ))}
