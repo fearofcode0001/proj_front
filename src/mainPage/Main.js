2c4b71b663de1b035b609e489346a3d3d733bc02
fearofgod0001 <dlxo8888@gmail.com>
2023-06-13 14:33:01 +0900

    0613

--- src/mainPage/Main.js
+++ src/mainPage/Main.js
@@ -198,7 +198,7 @@ const Chat =styled.div`
     bottom: 6rem;
     right: 2rem;
     background-color: rgba(255,255,255,0.8);
-    border-left: 1px solid black;
+    border-left: 2px solid #CCC;
     //그래야 안에 들어간 컨텐츠들이 사라진다.
     overflow: hidden;
     transition: height 0.35s ease;
