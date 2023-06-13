d83344d51dc4d1cd4d6a3a974bcc5db206f4cf4d
fearofgod0001 <dlxo8888@gmail.com>
2023-06-13 09:56:26 +0900

    0613

--- src/adminPage/Inventory.js
+++ src/adminPage/Inventory.js
@@ -5,13 +5,14 @@ import styled from "styled-components";
 const Container=styled.div`
 width: 100%;
 height: 100%;
-background-color: rgba(240,0,0,0.1);
 `
+
+
 const  Inventory = () =>{
     return(
 
         <Container>
-            Inventory
+           
         </Container>
     );
 };
