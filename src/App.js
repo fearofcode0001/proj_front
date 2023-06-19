c73848dc37bb12abb69a63207ee0d3eecd031a15
LeeSoYoung <258735@naver.com>
2023-06-19 15:03:27 +0900

    0619

--- src/App.js
+++ src/App.js
@@ -20,12 +20,13 @@ import Mypost from './pages/Mypost';
 import Order from './pages/Order';
 import Review from './pages/Review';
 import Wishlist from './pages/Wishlist';
+import test from './pages/test';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main/>}/>
+        {/* <Route path="/" element={<Main/>}/>
         <Route path="/Login" element={<Login/>}/>
         <Route path="/SignUp" element={<SignUp/>}/>        
         <Route path="/Cart" element={<Cart/>}/>
@@ -44,7 +45,8 @@ function App() {
         <Route path="/Order" element={<Order/>} />
         <Route path="/Mypost" element={<Mypost/>} />
         <Route path="/Review" element={<Review/>} />
-        <Route path="/Wishlist" element={<Wishlist/>} />
+        <Route path="/Wishlist" element={<Wishlist/>} /> */}
+        <Route path="/" element={<test/>} />
       </Routes>
      </Router>
 
