b61e57834fd656883de5742218b42e9a80174963
fearofgod0001 <dlxo8888@gmail.com>
2023-06-22 10:36:14 +0900

    0622_order

--- src/adminPage/OrderCheck.js
+++ src/adminPage/OrderCheck.js
@@ -1,6 +1,8 @@
-import React,{useContext} from "react";
+import React,{useContext,useState} from "react";
 import styled from "styled-components";
 import { UserContext } from "../context/UserInfo";
+import AxiosFinal from "../api/AxiosFinal";
+import { getValue } from "@testing-library/user-event/dist/utils";
 
 
 const Container=styled.table`
@@ -18,6 +20,12 @@ const OrderInfo=styled.tr`
     justify-content: space-around;
     align-items: center;    
     font-size: 11px;
+    .order{
+        width: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     .orderId{
         width: 70px;
         display: flex;
@@ -29,8 +37,13 @@ const OrderInfo=styled.tr`
         display: flex;
         justify-content: center;
     }
+    .orderDate{
+        width: 120px;
+        display: flex;
+        justify-content: center;
+    }
     .orderPrice{
-        width: 100px;
+        width: 50px;
         display: flex;
         justify-content: center;
     }
@@ -61,6 +74,22 @@ const OrderInfo=styled.tr`
             border: none;
         }
     }
+    .invoiceTrace{
+        width: 5px;
+        display: flex;
+        justify-content: center;
+        a{
+            
+            background-color: white;
+            color: black;
+            text-decoration: none;
+            font-size: 11px;
+            &:hover{
+                background-color: black;
+                color: white;
+            }
+        }
+    }
     .submitBtn{
         width: 50px;
         display: flex;
@@ -79,14 +108,52 @@ const OrderInfo=styled.tr`
     
 `
 const  OrderCheck = () =>{
-      //orderdata를 가져옴
-      const context = useContext(UserContext);
-      const {orderData} = context;
+    //orderdata를 가져옴
+    const context = useContext(UserContext);
+    const {orderData} = context;
+    //송장을 설정하는 useState
+    const [putShipCode,setPutShipCode] = useState();
+    //주문의 순서인 index를 설정하는 useState
+    const [orderIndex,setOrderIndex] = useState();
+    //송장 클릭시 index값이 들어와진다.
+    const onSetIndex=(index)=>{
+        setOrderIndex(index);
+    }
+    //들어와진 index 값의 orderData에서의 shipCode를 추출한다.
+    const onChangeShipCode=(e)=>{
+        console.log(e.target.value);
+        console.log(orderData[orderIndex].shipCode);
+        setPutShipCode(e.target.value);
+        console.log(putShipCode);       
+    }
     
+    const [orderStatue,SetOrderStatue] = useState();
+    const getValue = (e) => {
+        const { name } = e.target;
+        SetOrderStatue({
+            ...orderStatue,
+            //name 키를 가진 값을 value로 설정
+            [name]: e.target.value
+          })
+       
+    console.log(orderStatue);
+    // qnaSelect, qnaReply 각각 답이 담긴다. 
+    }
+
+
+    const onSubmitOrder =async(props)=>{
+        SetOrderStatue({...orderStatue});  
+        console.log(orderStatue);
+        // const response = AxiosFinal.qnaUploadReply(props);
+        // console.log("qna 답변 통신 ",response)
+    }
     return(
 
         <Container>
             <OrderInfo>
+                <div className="order">
+                    순서
+                </div>
                 <div className="orderId">
                     주문 ID
                 </div>
@@ -108,11 +175,17 @@ const  OrderCheck = () =>{
                 <div className="invoiceNum">
                     송장 번호
                 </div>
+                <div className="invoiceTrace">
+                    
+                </div>
                 <div className="submitBtn">
                     
                 </div>
             </OrderInfo> 
-            {orderData && orderData.map((o)=> <OrderInfo>
+            {orderData && orderData.map((o,index)=> <OrderInfo>
+                <div className="order">
+                    {index+1}
+                </div>
                 <div className="orderId">
                    {o.orderId}
                 </div>
@@ -126,7 +199,7 @@ const  OrderCheck = () =>{
                 {o.totalPrice}
                 </div>
                 <div className="orderStatus">
-                    <select name="orderStatus">
+                    <select name="orderStatus" onChange={getValue}>
                         <option value="" selected>{o.orderStatus}</option>
                         <option value="CHECK">주문 확인</option>
                         <option value="READY">상품 준비중</option>
@@ -139,18 +212,26 @@ const  OrderCheck = () =>{
                     
                 </div>
                 <div className="invoiceCom">
-                    <select name="">
+                    <select name='shipCompany' onChange={getValue}>
                         <option value="">{o.shipCompany}</option>
-                        <option value="">CJ대한통운</option>
-                        <option value="">롯데 택배</option>
-                        <option value="">한진 택배</option>
+                        <option value="CJ">CJ대한통운</option>
+                        <option value="LOTTE">롯데 택배</option>
+                        <option value="HANJIN">한진 택배</option>
                     </select>
                 </div>
                 <div className="invoiceNum">
-                    <input type="text" className="invoiceNum" value={o.shipCode}/>
+                    <input type="text" className="invoiceNum" value={putShipCode} placeholder={o.shipCode}
+                    onClick={()=>onSetIndex(index)} onChange={getValue} name='ShipInvoice'/>
+                </div>
+                <div className="invoiceTrace">
+                {o.shipCompany === null && <a href="#" target="blank">trace</a>}                
+                {o.shipCompany === "CJ" && <a href={'https://trace.cjlogistics.com/web/detail.jsp?slipno=/'+ o.shipCode} target="blank">trace</a>}
+                {o.shipCompany === "LOTTE" && <a href={'https://www.lotteglogis.com/home/reservation/tracking/linkView?InvNo=/'+ o.shipCode} target="blank">trace</a>}
+                {o.shipCompany === "HANJIN" && <a href={'https://smile.hanjin.co.kr:9080/eksys/smartinfo/m.html?wbl=/'+ o.shipCode} target="blank">trace</a>}
+                    {/* <a href={'https://trace.cjlogistics.com/web/detail.jsp?slipno=/'+ o.shipCode} target="blank">trace</a> */}
                 </div>          
                 <div className="submitBtn">
-                    <button>submit</button>
+                    <button onClick={()=>{onSubmitOrder(o.orderId)}}>submit</button>
                 </div>
             </OrderInfo>)}
 
